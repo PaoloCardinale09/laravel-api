104a0c509116aa859387780513c4674e956c17df
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-04-26 23:46:30 +0200

    add 404 page

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -34,9 +34,12 @@ class ProjectController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $project = Project::where('id', $id)->first();
+        if(!$project) return response(null, 404);
+
+        return response()->json($project);
     }
 
     /**
